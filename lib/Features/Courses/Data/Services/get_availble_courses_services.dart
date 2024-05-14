2f853b8b71df90959b32991a92b506050c068342
Eslam Tarek <eslamlef.308@gmail.com>
2024-05-14 04:15:19 +0300

    add get course selection api sercice

--- lib/Features/Courses/Data/Services/get_availble_courses_services.dart
+++ lib/Features/Courses/Data/Services/get_availble_courses_services.dart
@@ -1,14 +1,17 @@
 import 'dart:convert';
+
 import 'package:http/http.dart' as http;
-import 'package:flutter/material.dart';
 
 import '../../../../Data/API/Const/end_points.dart';
 import '../../../../Data/API/Token/token_manager.dart';
 import '../Models/availble_courses.dart';
+import '../Models/course_selection.dart';
 
 class ApiService {
   static const String _baseUrl = ApiConstants.baseUrl;
-  static const String _availableCoursesEndpoint = ApiConstants.courseSelectionAvailableCourses;
+  static const String _availableCoursesEndpoint =
+      ApiConstants.courseSelectionAvailableCourses;
+  static const String _courseSelectionEndpoint = ApiConstants.courseSelection;
 
   Future<AvailableCourses> fetchAvailableCourses() async {
     final response = await http.get(
@@ -22,6 +25,21 @@ class ApiService {
       return AvailableCourses.fromJson(jsonDecode(response.body));
     } else {
       throw Exception('Failed to load available courses');
+    }
+  }
+
+  Future<CourseSelection> fetchCourseSelection() async {
+    final response = await http.get(
+      Uri.parse('$_baseUrl$_courseSelectionEndpoint'),
+      headers: {
+        'Authorization': 'Bearer ${await TokenManager.getToken()}',
+      },
+    );
+
+    if (response.statusCode == 200) {
+      return CourseSelection.fromJson(jsonDecode(response.body));
+    } else {
+      throw Exception('Failed to load course selection');
     }
   }
 }
