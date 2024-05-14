16c6705b6586d5206c25d66962f37779a7c090dd
Eslam Tarek <eslamlef.308@gmail.com>
2024-05-14 04:55:19 +0300

    added enrolments end date

--- lib/Features/Courses/Presentation/View/Widgets/courses_minmax.dart
+++ lib/Features/Courses/Presentation/View/Widgets/courses_minmax.dart
@@ -1,49 +1,77 @@
 import 'package:flutter/material.dart';
 
+import '../../../Data/Models/course_selection.dart';
+import '../../../Data/Services/get_availble_courses_services.dart';
+
 // Define a model for the section data
 
-
-class CourseMinMax extends StatelessWidget {
+class CourseMinMax extends StatefulWidget {
   const CourseMinMax({super.key});
 
   @override
+  _CourseMinMaxState createState() => _CourseMinMaxState();
+}
+
+class _CourseMinMaxState extends State<CourseMinMax> {
+  late Future<CourseSelection> _courseSelection;
+
+  @override
+  void initState() {
+    super.initState();
+    _courseSelection = ApiService().fetchCourseSelection();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 190,
-      color: Colors.white,
-      padding: const EdgeInsets.all(20),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              _buildColumn('Courses', 'Courses'),
-              _buildColumn('Courses', 'Courses'),
-            ],
-          ),
-          const SizedBox(height: 20),
-          ElevatedButton(
-            onPressed: () {},
-            style: ElevatedButton.styleFrom(
-              fixedSize: const Size(200, 40),
-              backgroundColor: Colors.blueAccent,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10),
-              ),
+    return FutureBuilder<CourseSelection>(
+      future: _courseSelection,
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const CircularProgressIndicator();
+        } else if (snapshot.hasError) {
+          return Text('Error: ${snapshot.error}');
+        } else {
+          return Container(
+            height: 190,
+            color: Colors.white,
+            padding: const EdgeInsets.all(20),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    _buildColumn(
+                        'Min Credits: ${snapshot.data!.data!.attributes!.minCreditHours}',
+                        'Max Credits: ${snapshot.data!.data!.attributes!.maxCreditHours}'),
+                    _buildColumn('Courses', 'Courses'),
+                  ],
+                ),
+                const SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    fixedSize: const Size(200, 40),
+                    backgroundColor: Colors.blueAccent,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                  child: const Text(
+                    'Save',
+                    style: TextStyle(
+                      fontSize: 22,
+                      fontFamily: 'Outfit',
+                      fontWeight: FontWeight.normal,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ],
             ),
-            child: const Text(
-              'Save',
-              style: TextStyle(
-                fontSize: 22,
-                fontFamily: 'Outfit',
-                fontWeight: FontWeight.normal,
-                color: Colors.white,
-              ),
-            ),
-          ),
-        ],
-      ),
+          );
+        }
+      },
     );
   }
 
--- lib/Features/Courses/Presentation/View/Widgets/courses_upper_part.dart
+++ lib/Features/Courses/Presentation/View/Widgets/courses_upper_part.dart
@@ -2,79 +2,109 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
 import '../../../../../Constants/Icons.dart';
+import '../../../Data/Models/course_selection.dart';
+import '../../../Data/Services/get_availble_courses_services.dart';
+import 'package:intl/intl.dart';
 
-class CoursesScreenUpperPart extends StatelessWidget {
+DateTime dateTime = DateTime.parse('2024-03-26 02:08:00');
+String formattedDate = DateFormat('yyyy-MM-dd').format(dateTime);
+
+class CoursesScreenUpperPart extends StatefulWidget {
   const CoursesScreenUpperPart({super.key});
 
   @override
+  _CoursesScreenUpperPartState createState() => _CoursesScreenUpperPartState();
+}
+
+class _CoursesScreenUpperPartState extends State<CoursesScreenUpperPart> {
+  late Future<CourseSelection> _courseSelection;
+
+  @override
+  void initState() {
+    super.initState();
+    _courseSelection = ApiService().fetchCourseSelection();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(15.0),
-      child: Column(
-        children: [
-          const SizedBox(height: 15),
-          Row(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Column(
-                children: [
-                  const SizedBox(height: 20),
-                  SvgPicture.asset(
-                    userIconPath,
-                    height: 40,
-                    width: 40, // Corrected width
-                  ),
-                ],
-              ),
-              const SizedBox(width: 15),
-              const Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  SizedBox(height: 15),
-                  Text(
-                    'Eslam Tarek',
-                    style: TextStyle(
-                      fontSize: 30,
-                    ),
-                  ),
-                  Text(
-                    'Semester 2 Year 4', // Corrected spelling
-                    style: TextStyle(
-                      fontSize: 20,
-                    ),
-                  ),
-                ],
-              ),
-              const Expanded(child: SizedBox()), // Removed const
-              Column(
-                children: [
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  Container(
-                    width: 100, // Adjusted width for better visibility
-                    height: 80, // Reduced height for better proportion
-                    decoration: const BoxDecoration(
-                      borderRadius: BorderRadius.all(Radius.circular(10)),
-                      color: Colors.redAccent,
-                    ),
-                    child: const Center(
-                      child: Text(
-                        'Enrollments end on 20/20/2020', // Corrected spelling
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                          fontSize: 15,
-                          color: Colors.white,
+    return FutureBuilder<CourseSelection>(
+      future: _courseSelection,
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const CircularProgressIndicator();
+        } else if (snapshot.hasError) {
+          return Text('Error: ${snapshot.error}');
+        } else {
+          return Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: Column(
+              children: [
+                const SizedBox(height: 15),
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Column(
+                      children: [
+                        const SizedBox(height: 20),
+                        SvgPicture.asset(
+                          userIconPath,
+                          height: 40,
+                          width: 40,
                         ),
-                      ),
+                      ],
                     ),
-                  ),
-                ],
-              )
-            ],
-          ),
-        ],
-      ),
+                    const SizedBox(width: 15),
+                    const Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        SizedBox(height: 15),
+                        Text(
+                          'Eslam Tarek',
+                          style: TextStyle(
+                            fontSize: 30,
+                          ),
+                        ),
+                        Text(
+                          'Semester 2 Year 4',
+                          style: TextStyle(
+                            fontSize: 20,
+                          ),
+                        ),
+                      ],
+                    ),
+                    const Expanded(child: SizedBox()),
+                    Column(
+                      children: [
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        Container(
+                          width: 100,
+                          height: 80,
+                          decoration: const BoxDecoration(
+                            borderRadius: BorderRadius.all(Radius.circular(10)),
+                            color: Colors.redAccent,
+                          ),
+                          child: Center(
+                            child: Text(
+                              'Enrollments end on ${DateFormat('yyyy-MM-dd').format(DateTime.parse(snapshot.data!.data!.attributes!.endAt!))}',
+                              textAlign: TextAlign.center,
+                              style: const TextStyle(
+                                fontSize: 15 ,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              ],
+            ),
+          );
+        }
+      },
     );
   }
 }
