143bf4703d89b43262db4f80b1156cf9b5c2e67b
marco <emadmarko18@gmail.com>
2024-05-23 20:18:50 +0300

    total GPA in GPA Widget is done

--- lib/Features/Grades/presentation/manger/Academic_Year_Cubit/academic_year_state.dart
+++ lib/Features/Grades/presentation/manger/Academic_Year_Cubit/academic_year_state.dart
@@ -1,18 +1,18 @@
 import 'package:eductionsystem/Features/Grades/data/models/academic_year_model.dart';
 
-abstract class AcademicYearStates{}
+abstract class AcademicYearStates {}
 
+class InitialAcademicYearState extends AcademicYearStates {}
 
-
-
-class InitialAcademicYearState extends AcademicYearStates{}
-class SuccessAcademicYearState extends AcademicYearStates{
+class SuccessAcademicYearState extends AcademicYearStates {
   final List<AcademicYearModel> yearsList;
 
   SuccessAcademicYearState({required this.yearsList});
 }
-class LoadingAcademicYearState extends AcademicYearStates{}
-class FailureAcademicYearState extends AcademicYearStates{
+
+class LoadingAcademicYearState extends AcademicYearStates {}
+
+class FailureAcademicYearState extends AcademicYearStates {
   final String errMsg;
 
   FailureAcademicYearState({required this.errMsg});
