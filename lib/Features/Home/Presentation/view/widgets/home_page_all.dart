780ffc3bd22e4cba915432565f5afb15661b7f8f
marco <emadmarko18@gmail.com>
2024-05-28 23:29:35 +0300

    add photo to home page

--- lib/Features/Home/Presentation/view/widgets/home_page_all.dart
+++ lib/Features/Home/Presentation/view/widgets/home_page_all.dart
@@ -3,9 +3,49 @@ import 'package:flutter_svg/flutter_svg.dart';
 
 import '../../../../../Constants/FontsConst.dart';
 import '../../../../../Constants/Icons.dart';
+import '../../../../../Data/API/Models/user_data.dart';
+import '../../../../../Data/API/Services/auth_service.dart';
+import '../../../../../Data/API/Token/token_manager.dart';
 
-class HomePageAll extends StatelessWidget {
+class HomePageAll extends StatefulWidget {
   const HomePageAll({super.key});
+
+  @override
+  _HomePageAllState createState() => _HomePageAllState();
+}
+
+class _HomePageAllState extends State<HomePageAll> {
+  final AuthRepository _authRepository = AuthRepository(
+    authApi: AuthApi(baseUrl: 'http://10.0.2.2:8000'),
+  );
+  UserDataModel? _userData;
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchUserData();
+  }
+
+  Future<void> _fetchUserData() async {
+    try {
+      final token = await TokenManager.getToken();
+      if (token != null) {
+        final userData = await _authRepository.fetchUserData(token);
+        if (userData != null) {
+          print('Fetched user data: ${userData.toJson()}'); // Debug statement
+          setState(() {
+            _userData = userData;
+          });
+        } else {
+          print('User data is null');
+        }
+      } else {
+        print('Token is null');
+      }
+    } catch (error) {
+      print('Error fetching user data: $error');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -23,32 +63,33 @@ class HomePageAll extends StatelessWidget {
                     mainAxisAlignment: MainAxisAlignment.start,
                     children: [
                       const SizedBox(width: 15),
-                      Column(
+                      Column(mainAxisAlignment: MainAxisAlignment.center,
                         children: [
-                          SvgPicture.asset(
-                            userIconPath,
-                            height: 35,
-                            width: 3,
+                          SizedBox(height: 5),
+                          CircleAvatar(
+                            radius: 20,
+                            backgroundColor: Colors.grey[300],
+                            backgroundImage: _userData != null && _userData!.data!.attributes!.image != null
+                                ? NetworkImage('http://10.0.2.2:8000/storage/${_userData!.data!.attributes!.image}')
+                                : null,
                           ),
-                          const SizedBox(
-                            height: 20,
-                          )
+                          const SizedBox(height: 20),
                         ],
                       ),
                       const SizedBox(width: 10),
-                      const Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-
-                              'Username',
-                              style: AppFonts.manrope18,
-                            ),
-                            Text(
-                              'You Enrolled in AYA',
-                              style: AppFonts.manrope15,
-                            ),
-                          ]),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            _userData?.data?.attributes?.name ?? 'name',
+                            style: AppFonts.manrope18,
+                          ),
+                          const Text(
+                            'You Enrolled in AYA',
+                            style: AppFonts.manrope15,
+                          ),
+                        ],
+                      ),
                       const Expanded(child: SizedBox()),
                       Column(
                         children: [
@@ -59,9 +100,7 @@ class HomePageAll extends StatelessWidget {
                           ),
                         ],
                       ),
-                      const SizedBox(
-                        width: 30,
-                      )
+                      const SizedBox(width: 30),
                     ],
                   ),
                 ],
